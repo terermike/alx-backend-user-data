7a7fb69b2e7a63051e85e7b91f5c9ef13119219b
Terer <terermike3@gmail.com>
2023-06-07 14:32:34 +0300

    Made changes to make the module less complex

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -3,7 +3,6 @@
 
 from typing import Optional
 from sqlalchemy import Column, Integer, String
-from typing import Optional
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -22,28 +21,3 @@ class User(Base):
     def __repr__(self) -> str:
         """Returns a string representation of the User object"""
         return f"User: id={self.id}"
-
-    @staticmethod
-    def authenticate(email: str, password: str) -> Optional['User']:
-        """
-        Authenticates a user by matching the email and hashed password.
-        Returns the User object if authentication is successful, otherwise
-        None.
-        """
-        # Logic for authenticating the user
-        # Example implementation:
-        user = User.query.filter_by(email=email, hashed_password=password).\
-            first()
-        return user
-
-    def set_session_id(self, session_id: str) -> None:
-        """Sets the session ID for the user"""
-        self.session_id = session_id
-
-    def reset_password(self, reset_token: str, new_password: str) -> None:
-        """Resets the user's password using a reset token"""
-        # Logic for resetting the password
-        # Example implementation:
-        if self.reset_token == reset_token:
-            self.hashed_password = new_password
-            self.reset_token = None
